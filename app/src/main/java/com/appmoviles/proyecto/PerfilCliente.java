6bcf68af72b3ec4b08928b0938acc21b30dd2d72
alejandraRamos <49355144+alejandraRamos@users.noreply.github.com>
2019-05-10 12:14:48 -0500

    mi perfil

--- app/src/main/java/com/appmoviles/proyecto/PerfilCliente.java
+++ app/src/main/java/com/appmoviles/proyecto/PerfilCliente.java
@@ -1,6 +1,7 @@
 package com.appmoviles.proyecto;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,6 +21,12 @@ public class PerfilCliente extends Fragment {
     private TextView tv_telefono;
     private ImageView image_atras;
 
+    private TextView tv_micuenta_perfil_cliente;
+    private TextView tv_ajustes_perfil_cliente;
+    private TextView tv_ayuda_perfil_cliente;
+    private TextView tv_informacion_perfil_cliente;
+
+    Context contexto;
 
     public PerfilCliente() {
 
@@ -36,11 +43,21 @@ public class PerfilCliente extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v= inflater.inflate(R.layout.fragment_perfil_cliente, container, false);
+        contexto = v.getContext();
 
         tv_nombre_iniciales = v.findViewById(R.id.iv_nombre_perfil_cliente);
         tv_nombre= v.findViewById(R.id.tv_nombre_cliente_perfil_cliente);
         tv_telefono= v.findViewById(R.id.tv_numero_telefono_perfil_cliente);
         image_atras = v.findViewById(R.id.iv_fragment_perfil_clientes);
+
+        tv_micuenta_perfil_cliente = v.findViewById(R.id.tv_micuenta_perfil_cliente);
+        tv_micuenta_perfil_cliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(contexto, MiCuentaPerfil.class );
+                startActivity(i);
+            }
+        });
 
 
 
