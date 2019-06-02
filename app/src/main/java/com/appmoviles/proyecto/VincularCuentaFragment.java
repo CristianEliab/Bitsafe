637cf64a242d2e0dcfb5e3d61f5a5a72fb7c94ab
alejandraRamos <49355144+alejandraRamos@users.noreply.github.com>
2019-06-02 15:41:22 -0500

    Listo calendario y spinners

--- app/src/main/java/com/appmoviles/proyecto/VincularCuentaFragment.java
+++ app/src/main/java/com/appmoviles/proyecto/VincularCuentaFragment.java
@@ -4,18 +4,30 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.appmoviles.proyecto.modelo.Banco;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Calendar;
+
+import static com.appmoviles.proyecto.util.Constantes.CHILD_BANCOS;
 
 public class VincularCuentaFragment extends Fragment implements Serializable {
 
@@ -25,6 +37,8 @@ public class VincularCuentaFragment extends Fragment implements Serializable {
     private LinearLayout ly_agregar_fecha_cuenta;
     private TextView et_date_cuenta;
 
+    FirebaseDatabase database;
+    FirebaseAuth auth;
     private int mYear, mMonth, mDay;
 
     public VincularCuentaFragment(){}
@@ -40,13 +54,46 @@ public class VincularCuentaFragment extends Fragment implements Serializable {
                              Bundle savedInstanceState) {
         View v= inflater.inflate(R.layout.fragment_vincular_cuenta, container, false);
 
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
         spinner_cuenta_tipo = v.findViewById(R.id.spinner_cuenta_tipo);
+
+
+
         spinner_banco_cuenta=v.findViewById(R.id.spinner_banco_cuenta);
+
+        final ArrayList<CharSequence> bancos = new ArrayList<>();
+        database.getReference().child(CHILD_BANCOS).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Banco bancoTmp;
+                for (DataSnapshot hijo: dataSnapshot.getChildren()){
+                    bancoTmp=hijo.getValue(Banco.class);
+                    bancos.add(bancoTmp.getNombreBanco());
+
+                    ArrayAdapter<CharSequence> bancosLista = new ArrayAdapter<>(getContext(), R.layout.spinner_item_banco,bancos);
+                    spinner_banco_cuenta.setAdapter(bancosLista);
+                    //ahora
+                    String seleccionBanco = spinner_banco_cuenta.getSelectedItem().toString();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
         tv_numero_cuenta = v.findViewById(R.id.tv_numero_cuenta);
+
+
+
+
+
         ly_agregar_fecha_cuenta = v.findViewById(R.id.ly_agregar_fecha_cuenta);
         et_date_cuenta =v.findViewById(R.id.et_date_cuenta);
-
-
         ly_agregar_fecha_cuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
